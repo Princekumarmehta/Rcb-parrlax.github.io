28ac0e170df4dce9113a01dad5220eb674d493da
Prince Kumar <109470447+Princekumarmehta@users.noreply.github.com>
2023-03-30 11:41:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ $('.partners').hover(
     function () { $(' .part_text').toggleClass('animate__flipInX') }
     // function () { $(this).removeClass('animate__fadeInRightBig') }
 );
+setTimeout(function () { alert("For discription check 'HOME' page from Nav-bar ") }, 5000);
